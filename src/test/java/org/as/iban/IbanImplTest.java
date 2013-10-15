dcfac1ce001dc7b5b7efba4b21cfadc630972fd4
RGierse <ralf.gierse@aventum-solutions.de>
2013-10-15 16:58:16 +0200

    countries added in JUnit Test ASP-23

--- src/test/java/org/as/iban/IbanImplTest.java
+++ src/test/java/org/as/iban/IbanImplTest.java
@@ -20,24 +20,98 @@ public class IbanImplTest {
     @Test
     public void testIbanImplStringStringString() {
 	
-	// German
-	// Standardrule
-	String blz = "37040044";
-	String kto = "532013000";
-	Iban ibanGerman = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, blz, kto);
-	assertEquals("DE89370400440532013000", ibanGerman.toString());
+	String bankIdent;
+	String ktoIdent;
+	Iban iban;
+	
+	/*
+	 * Austria
+	 */
+	bankIdent = "19043";
+	ktoIdent = "234573201";
+	iban = new IbanImpl(Iban.COUNTRY_CODE_AUSTRIA, bankIdent, ktoIdent);
+	assertEquals("AT611904300234573201", iban.toString());
+	
+	/*
+	 * Belgium
+	 */
+	bankIdent = "539";
+	ktoIdent = "7547034";
+	iban = new IbanImpl(Iban.COUNTRY_CODE_BELGIUM, bankIdent, ktoIdent);
+	assertEquals("BE68539007547034", iban.toString());
+	
+	/*
+	 * Bulgaria
+	 */
+	bankIdent = "BNBG9661";
+	ktoIdent = "1020345678";
+	iban = new IbanImpl(Iban.COUNTRY_CODE_BULGARIA, bankIdent, ktoIdent);
+	assertEquals("BG80BNBG96611020345678", iban.toString());
+	
+	/*
+	 * Croatia
+	 */
+	bankIdent = "1001005";
+	ktoIdent = "1863000160";
+	iban = new IbanImpl(Iban.COUNTRY_CODE_CROATIA, bankIdent, ktoIdent);
+	assertEquals("HR1210010051863000160", iban.toString());
+	
+	/* 
+	 * German
+	 */
+	// Regel 0000
+	bankIdent = "37040044";
+	ktoIdent = "532013000";
+	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+	assertEquals("DE89370400440532013000", iban.toString());
     }
 
     @Test
     public void testValidate() {
 
-	// German
-	// Standardrule
-	Iban ibanGermanValid = new IbanImpl("DE89370400440532013000");
-	Iban ibanGermanInvalid = new IbanImpl("DE89370400440532013001");
+	Iban ibanValid;
+	Iban ibanInvalid;
+
+	/*
+	 * Austria
+	 */
+	ibanValid = new IbanImpl("AT611904300234573201");
+	ibanInvalid = new IbanImpl("AT611904300234573200");
+	assertTrue(ibanValid.validate());
+	assertFalse(ibanInvalid.validate());
 	
-	assertTrue(ibanGermanValid.validate());
-	assertFalse(ibanGermanInvalid.validate());
+	/*
+	 * Belgium
+	 */
+	ibanValid = new IbanImpl("BE68539007547034");
+	ibanInvalid = new IbanImpl("BE68539007547035");
+	assertTrue(ibanValid.validate());
+	assertFalse(ibanInvalid.validate());
+	
+	/*
+	 * Bulgaria
+	 */
+	ibanValid = new IbanImpl("BG80BNBG96611020345678");
+	ibanInvalid = new IbanImpl("BG80BNBG96611020345679");
+	assertTrue(ibanValid.validate());
+	assertFalse(ibanInvalid.validate());
+	
+	/*
+	 * Croatia
+	 */
+	ibanValid = new IbanImpl("HR1210010051863000160");
+	ibanInvalid = new IbanImpl("HR1210010051863000161");
+	assertTrue(ibanValid.validate());
+	assertFalse(ibanInvalid.validate());
+
+	/* 
+	 * German
+	 */
+	// Regel 0000
+	ibanValid = new IbanImpl("DE89370400440532013000");
+	ibanInvalid = new IbanImpl("DE89370400440532013001");
+	assertTrue(ibanValid.validate());
+	assertFalse(ibanInvalid.validate());
     }
 
 }
