c027195e484bd79f4ec4e9bb77b6253bf6098781
daniel <daniel.klein@aventum-solutions.de>
2013-10-23 15:19:39 +0200

    Regel 005300

--- src/test/java/org/as/iban/IbanValidatorGermanTest.java
+++ src/test/java/org/as/iban/IbanValidatorGermanTest.java
@@ -65,4 +65,55 @@ public class IbanValidatorGermanTest {
     	assertEquals("DE92210602370012796743", iban.toString());
     }
 
+    @Test
+    public void rule005300()
+    {
+    	//	BLZ 55050000
+    	bankIdent = "55050000";
+    	ktoIdent = "35000";
+    	
+    	Iban iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE94600501017401555913", iban.toString());
+    	
+    	//	BLZ 60020030
+    	bankIdent = "60020030";
+    	ktoIdent = "1003340500";
+    	
+    	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE06600501010002001155", iban.toString());
+    	
+    	// BLZ 60050000
+    	bankIdent = "60050000";
+    	ktoIdent = "3002";
+    	
+    	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE23600501010001367924", iban.toString());
+    	
+    	// BLZ 66020020
+    	bankIdent = "66020020";
+    	ktoIdent = "4002015800";
+    	
+    	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE02600501017495530102", iban.toString());
+    	
+    	// BLZ 66050000
+    	bankIdent = "66050000";
+    	ktoIdent = "85304";
+    	
+    	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE15600501017402045439", iban.toString());
+    	
+    	// BLZ 86050000
+    	bankIdent = "86050000";
+    	ktoIdent = "3535";
+    	
+    	iban = new IbanImpl(Iban.COUNTRY_CODE_GERMAN, bankIdent, ktoIdent);
+    	
+    	assertEquals("DE61600501017461505611", iban.toString());
+    }
 }
